f0aac21f28b493c3602515c4eecb499f1fbf4b68
rahmaarhs <122796890+rahmaarhs@users.noreply.github.com>
2023-01-18 09:19:42 +0700

    Add files via upload

---
+++ head.h
@@ -0,0 +1,18 @@
+#include <conio.h>
+#include <iostream.h>
+
+head(char jud[50]=""){
+	cout<<"***************************************************************"<<endl;
+   cout<<".....................::PEGADAIAN SYARIAH::....................."<<endl;
+	cout<<"                       "<<jud<<endl;
+  	cout<<"***************************************************************"<<endl;
+   if(strcmp(jud,"LOGIN APLIKASI")==0){
+    	cout<<"JUDUL     : LOGIN"<<endl;
+   	cout<<"PEMBUAT   : rahmar"<<endl;
+   	cout<<"KELAS     : 10 pplg"<<endl;
+   	cout<<"SEMESTER  : 2"<<endl;
+      cout<<"NOTE      : Gunakan (Username:rahmaa) dan (Password:aiueold)"<<endl;
+   	cout<<"***************************************************************"<<endl;
+    	cout<<"***************************************************************"<<endl;
+   }
+}
